49bf64c388aa5d665e248a58f146e0f9d143a9cd
Shubham Shaswat <98876054+TapItNinja@users.noreply.github.com>
2025-06-02 19:14:56 +0530

    Time: 5 ms (5.76%), Space: 17 MB (6.52%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+        //POWER SET
+    // vector<vector<int>> subsets(vector<int>& nums) {
+    //     int n=nums.size();
+    //     int sub=1<<n;
+    //     vector<vector<int>> ans;
+    //     for(int i=0; i<sub; i++){
+    //         vector<int>subs;
+    //         for(int j=0; j<n; j++){
+    //             if(i & (1<<j)){
+    //                 subs.push_back(nums[j]);
+    //             }
+    //         }
+    //         ans.push_back(subs);
+    //     }
+    //     return ans;
+    // }
+
     void solve(int idx,vector<int>emp,  vector<int>sub, vector<vector<int>>&ans, int n){
         if(idx>=n){
             ans.push_back(emp);
