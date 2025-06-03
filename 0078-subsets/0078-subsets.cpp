1b51c99ef171e5a2140822f1f744ebb1ba7f5ffd
Shubham Shaswat <98876054+TapItNinja@users.noreply.github.com>
2025-06-04 01:00:50 +0530

    Time: 0 ms (100%), Space: 10 MB (60.96%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,38 +1,18 @@
 class Solution {
 public:
-        //POWER SET
-    // vector<vector<int>> subsets(vector<int>& nums) {
-    //     int n=nums.size();
-    //     int sub=1<<n;
-    //     vector<vector<int>> ans;
-    //     for(int i=0; i<sub; i++){
-    //         vector<int>subs;
-    //         for(int j=0; j<n; j++){
-    //             if(i & (1<<j)){
-    //                 subs.push_back(nums[j]);
-    //             }
-    //         }
-    //         ans.push_back(subs);
-    //     }
-    //     return ans;
-    // }
-
-    void solve(int idx,vector<int>emp,  vector<int>sub, vector<vector<int>>&ans, int n){
-        if(idx>=n){
-            ans.push_back(emp);
-            return;
-        }
-        emp.push_back(sub[idx]);
-        solve(idx+1,emp, sub, ans, n);
-
-        emp.pop_back();
-        solve(idx+1,emp, sub, ans, n);
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>ans;
-        vector<int>emp;
-        solve(0,emp, nums, ans, n);
-        return ans;
+        int sub=1<<n;
+        vector<vector<int>>final;
+        for(int i=0; i<=sub-1; i++){
+            vector<int>ans;
+            for(int j=0; j<=n-1; j++){
+                if(i & (1<<j)){
+                    ans.push_back(nums[j]);
+                }
+        }
+        final.push_back(ans);
+        }
+        return final;
     }
 };
