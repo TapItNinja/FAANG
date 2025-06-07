ed62b68c6deef4d7a0be4465def67cd0aee1ec76
Shubham Shaswat <98876054+TapItNinja@users.noreply.github.com>
2025-06-07 15:28:19 +0530

    Time: 12 ms (17.82%), Space: 13.5 MB (19.27%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> sym = {
-    {'I', 1},
-    {'V', 5},
-    {'X', 10},
-    {'L', 50},
-    {'C', 100},
-    {'D', 500},
-    {'M', 1000}
-};
-    int result=0;
-    int n=s.size();
-    for(int i=0; i<n; i++){
-        if(sym[s[i]]<sym[s[i+1]] && i<n-1){
-            result-=sym[s[i]];
-        }else{
-            result+=sym[s[i]];
+        unordered_map<char, int>mpp={
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+            };
+            int sum=0;
+            int n=s.size();
+            for(int i=0; i<n; i++){
+                if(i+1<n && mpp[s[i]]<mpp[s[i+1]]){
+                    sum-=mpp[s[i]];
+                }else{
+                    sum+=mpp[s[i]];
+                }
+            }
+            return sum;
         }
-    }
-    return result;
-    }
 };
