e2bb91635d6b9d0c7730ea8027680e58aa298a76
Shubham Shaswat <98876054+TapItNinja@users.noreply.github.com>
2025-06-10 23:52:46 +0530

    Time: 1 ms (80.16%), Space: 10.2 MB (54.99%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    void solve(int col, vector<vector<string>>&ans, vector<string>&board, vector<int>&Row, vector<int>&upperd, vector<int>&lowerd, int n){
+    void solve(int col, vector<vector<string>>&ans, vector<string>&board, vector<int>&Row, vector<int>&UpperD, vector<int>&LowerD, int n){
         if(col==n){
             ans.push_back(board);
             return;
         }
         for(int row=0; row<n; row++){
-            if(Row[row]==0 && upperd[row+col]==0 && lowerd[n-1+col-row]==0){
-                board[col][row]='Q';
+            if(Row[row]==0 && UpperD[row+col]==0 && LowerD[n-1+col-row]==0){
+                board[row][col]='Q';
                 Row[row]=1;
-                upperd[row+col]=1;
-                lowerd[n-1+col-row]=1;
-                solve(col+1, ans, board, Row, upperd, lowerd, n);
-                board[col][row]='.';
+                UpperD[row+col]=1;
+                LowerD[n-1+col-row]=1;
+                solve(col+1, ans, board, Row, UpperD, LowerD, n);
+                board[row][col]='.';
                 Row[row]=0;
-                upperd[row+col]=0;
-                lowerd[n-1+col-row]=0;
+                UpperD[row+col]=0;
+                LowerD[n-1+col-row]=0;
             }
         }
     }
@@ -26,8 +26,8 @@ public:
         for(int i=0; i<n; i++){
             board[i]=s;
         }
-        vector<int>ROW(n,0), UPPERD(2*n-1, 0), LOWERD(2*n-1, 0);
-        solve(0, ans, board, ROW, UPPERD, LOWERD, n);
+        vector<int>Row(n, 0), UpperD(2*n-1, 0), LowerD(2*n-1, 0);
+        solve(0, ans, board, Row, UpperD, LowerD, n);
         return ans;
     }
 };
