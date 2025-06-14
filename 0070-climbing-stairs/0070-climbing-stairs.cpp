fd0d3b6207e232fbed37927c0fb4a2643f370888
Shubham Shaswat <98876054+TapItNinja@users.noreply.github.com>
2025-06-14 18:14:21 +0530

    Time: 0 ms (100%), Space: 8.8 MB (17.55%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,24 +1,14 @@
-//Memoization
-
 class Solution {
 public:
-    int f(int n, vector<int>&dp){
+    int solve(int n, vector<int>&dp){
         if(n==0)return 1;
-        if(n==1)return 1;
-        if(dp[n]!=-1)return dp[n];
-        return dp[n]=f(n-1, dp)+f(n-2, dp);
+        if(n<0)return 0;
+        if(dp[n]!=-1){return dp[n];}
+        return dp[n]=solve(n-1, dp)+solve(n-2, dp);
+        
     }
     int climbStairs(int n) {
-        
-        int prev2=1;
-        int prev1=1;
-        int curr=1;
-        for(int i=2; i<=n; i++){
-            curr=prev1+prev2;
-            prev2=prev1;
-            prev1=curr;
-        }
-        
-        return prev1;
+        vector<int>dp(n+1, -1);
+        return solve(n, dp);
     }
 };
