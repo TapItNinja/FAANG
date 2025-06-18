d1846343a93a417adae4838b9df0e6a9b6bce265
Shubham Shaswat <98876054+TapItNinja@users.noreply.github.com>
2025-06-18 15:17:54 +0530

    Time: 0 ms (100%), Space: 9.3 MB (57.9%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    void solve(int i, unordered_map<char, string> &dt, string digits, vector<string>&ans, int n, string &mid){
-        if(i==n){
+    void solve(int idx, int n, string &digits, unordered_map<char, string>&dt, string mid, vector<string>&ans){
+        if(idx==n){
             ans.push_back(mid);
             return;
         }
-        char digit=digits[i];
+        char digit=digits[idx];
         for(auto& ch: dt[digit]){
-            mid.push_back(ch);
-            solve(i+1, dt, digits, ans, n, mid);
+            mid+=ch;
+            solve(idx+1, n, digits, dt, mid, ans);
             mid.pop_back();
         }
-
     }
     vector<string> letterCombinations(string digits) {
         int n=digits.size();
         if(n==0)return {};
-        unordered_map<char, string> dt = {
-    {'2', "abc"},
-    {'3', "def"},
-    {'4', "ghi"},
-    {'5', "jkl"},
-    {'6', "mno"},
-    {'7', "pqrs"},
-    {'8', "tuv"},
-    {'9', "wxyz"}
-    };
-    vector<string>ans;
-    string mid="";
-    solve(0, dt, digits, ans, n, mid);
-    return ans;
+        vector<string>ans;
+        unordered_map<char, string>dt={
+            {'2', "abc"},
+            {'3', "def"},
+            {'4', "ghi"},
+            {'5', "jkl"},
+            {'6', "mno"},
+            {'7', "pqrs"},
+            {'8', "tuv"},
+            {'9', "wxyz"}
+        };
+        string mid="";
+        solve(0, n, digits, dt, mid, ans);
+        return ans;
 
     }
 };
