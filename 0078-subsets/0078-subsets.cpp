5622d508d2ae3449c4f6bb16f5f040ee11916074
Shubham Shaswat <98876054+TapItNinja@users.noreply.github.com>
2025-06-24 00:40:00 +0530

    Time: 3 ms (32.73%), Space: 9.8 MB (81.27%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    void solve(int idx, int n, vector<vector<int>>&ans, vector<int>&mid, vector<int>& nums){
-        if(idx==n){
-            ans.push_back(mid);
-            return;
-        }
-        mid.push_back(nums[idx]);
-        solve(idx+1, n, ans, mid, nums);
-        mid.pop_back();
-        solve(idx+1, n, ans, mid, nums);
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
         int n=nums.size();
+        int subs=1<<n;
         vector<vector<int>>ans;
-        vector<int>mid;
-        solve(0, n, ans, mid, nums);
+        for(int i=0; i<=subs-1; i++){
+            vector<int>combo;
+            for(int j=0; j<=n-1; j++){
+                if(i & (1<<j))combo.push_back(nums[j]);
+            }
+            ans.push_back(combo);
+        }
         return ans;
     }
 };
