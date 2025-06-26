3fdf52556e71a8efb1eff253a8cc2f5aa12b51bc
Shubham Shaswat <98876054+TapItNinja@users.noreply.github.com>
2025-06-26 18:41:19 +0530

    Time: 0 ms (100%), Space: 17.5 MB (52.38%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    int solve2(int n, vector<int>&nums, int target){
-        int right=n-1;
+    int solve1(int n, vector<int>& nums, int target){
         int left=0;
-        int last=-1;
+        int right=n-1;
+        int point=-1;
         while(left<=right){
             int mid=left+(right-left)/2;
-            if(nums[mid]==target){
-                last=mid;
-                left=mid+1;
-            }else if(nums[mid]<target)left=mid+1;
+            if(nums[mid]==target){point=mid;left=mid+1; }
+            else if(nums[mid]<target)left=mid+1;
             else right=mid-1;
         }
-        return last;
+        return point;
     }
-    int solve1(int n, vector<int>&nums, int target){
-        int right=n-1;
+    int solve2(int n, vector<int>& nums, int target){
         int left=0;
-        int first=-1;
+        int right=n-1;
+        int point=-1;
         while(left<=right){
             int mid=left+(right-left)/2;
-            if(nums[mid]==target){
-                first=mid;
-                right=mid-1;
-            }else if(nums[mid]<target)left=mid+1;
+            if(nums[mid]==target){point=mid;right=mid-1; }
+            else if(nums[mid]<target)left=mid+1;
             else right=mid-1;
         }
-        return first;
+        return point;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
         int n=nums.size();
-        int left=solve1(n, nums, target);
-        int right=solve2(n, nums, target);
-        return {left, right};
+        int right=solve1(n, nums, target);
+        int left=solve2(n, nums, target);
+        return {left ,right};
     }
 };
