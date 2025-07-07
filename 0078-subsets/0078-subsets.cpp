8c65af66e534c6058409d3ec8a63b0ff4f1df433
Shubham Shaswat <98876054+TapItNinja@users.noreply.github.com>
2025-07-07 22:55:17 +0530

    Time: 3 ms (32.82%), Space: 9.9 MB (60.92%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         int n=nums.size();
-        int subs=1<<n;
+        int power=1<<n;
         vector<vector<int>>ans;
-        for(int i=0; i<=subs-1; i++){
-            vector<int>combo;
-            for(int j=0; j<=n-1; j++){
-                if(i & (1<<j))combo.push_back(nums[j]);
+        for(int i=0; i<power; i++){
+            vector<int>push;
+            for(int j=0; j<n; j++){
+                if(((1<<j) & i)){
+                    push.push_back(nums[j]);
+                }
             }
-            ans.push_back(combo);
+            ans.push_back(push);
         }
         return ans;
     }
