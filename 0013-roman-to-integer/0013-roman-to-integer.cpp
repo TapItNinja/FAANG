8e73dd06c0f9ed49da2a1e7462f7d9204d32ea2b
Shubham Shaswat <98876054+TapItNinja@users.noreply.github.com>
2025-08-05 22:06:17 +0530

    Time: 4 ms (66.9%), Space: 13.3 MB (49.14%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -9,16 +9,18 @@ public:
             {'C', 100},
             {'D', 500},
             {'M', 1000}
-            };
-            int sum=0;
-            int n=s.size();
-            for(int i=0; i<n; i++){
-                if(i+1<n && mpp[s[i]]<mpp[s[i+1]]){
-                    sum-=mpp[s[i]];
-                }else{
-                    sum+=mpp[s[i]];
-                }
+        };
+        int sum=0;
+        int n=s.size();
+        for(int i=0; i<n; i++){
+            if(i+1<n && mpp[s[i+1]]>mpp[s[i]] ){
+                sum+=mpp[s[i+1]]-mpp[s[i]];
+                i++;
             }
-            return sum;
+            else{
+                sum+=mpp[s[i]];
+            }
         }
+        return sum;
+    }
 };
