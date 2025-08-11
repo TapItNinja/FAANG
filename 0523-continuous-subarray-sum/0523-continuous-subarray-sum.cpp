e8d88a9e92bad80ab90af86bdd7ec0694f7a98c2
Shubham Shaswat <98876054+TapItNinja@users.noreply.github.com>
2025-08-11 07:21:12 +0530

    Time: 151 ms (11.07%), Space: 147.9 MB (21.49%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
+        int sum=0;
+        int len=0;
+        map<int, int>mpp;
+        mpp[0]=-1;
+        for(int i=0; i<n; i++){
+            sum+=nums[i];
+            if(sum%k==0){
+                if(i-mpp[0]>1)return true;
+            }
+            int rem=sum%k;
+            if(mpp.find(rem)!=mpp.end()){
+                if(i-mpp[rem]>1)return true;
+            }else mpp[sum%k]=i;
+        }
+        return false;
+    }
+};
