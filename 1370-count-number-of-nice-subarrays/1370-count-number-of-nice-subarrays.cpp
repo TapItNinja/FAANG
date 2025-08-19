dbdc874d63167286da0d87476b32a4ac633e8874
Shubham Shaswat <98876054+TapItNinja@users.noreply.github.com>
2025-08-19 22:06:16 +0530

    Time: 4 ms (82.98%), Space: 71.4 MB (92.94%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    bool odd1(int n){
-        if(n%2==0)return false;
-        return true;
+    bool odd(int n){
+        if(n%2!=0)return true;
+        return false;
     }
-    int solve(vector<int>&nums, int goal){
-        int r=0, l=0, count=0, odd=0, numof=0;
-        while(r<nums.size()){
-            if(odd1(nums[r]))count++;
-            while(count>goal){
-                if(odd1(nums[l]))count--;
+    int solve(vector<int>&nums, int k){
+        int r=0, l=0;
+        int combo=0;
+        int n=nums.size();
+        int count=0;
+        while(r<n){
+            if(odd(nums[r])){
+                count++;
+            }
+            while(count>k){
+                if(odd(nums[l]))count--;
                 l++;
             }
-            numof=numof+(r-l+1);
+            combo+=(r-l+1);
             r++;
         }
-        return numof;
+        return combo;
     }
     int numberOfSubarrays(vector<int>& nums, int k) {
         return solve(nums, k)-solve(nums, k-1);
